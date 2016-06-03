121ec4cc9a95583458cb8fe848ef5c60317507f7
Alonso <code@zhangwu.space>
2016-06-03 11:22:14 +0800

    Add Cocoapods

---
+++ Class/Extension/Color+Extension.swift
@@ -0,0 +1,23 @@
+//
+//  Color+Extension.swift
+//  NewBee
+//
+//  Created by Alonso Zhang on 16/6/3.
+//  Copyright © 2016年 Alonso Zhang. All rights reserved.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    class func colorWithCustom(r: CGFloat, g:CGFloat, b:CGFloat) -> UIColor {
+        return UIColor(red: r / 255.0, green: g / 255.0, blue: b / 255.0, alpha: 1)
+    }
+    
+    class func randomColor() -> UIColor {
+        let r = CGFloat(arc4random_uniform(256))
+        let g = CGFloat(arc4random_uniform(256))
+        let b = CGFloat(arc4random_uniform(256))
+        return UIColor.colorWithCustom(r, g: g, b: b)
+    }
+}
